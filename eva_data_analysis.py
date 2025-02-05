cd964e5eba1bbe6fe9489368ef14f373a3d94607
kteleg <telegraph@novamechanics.com>
2025-02-05 16:09:55 +0200

    Added some informative comments

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -8,12 +8,17 @@ graph_file = './cumulative_eva_graph.png'
 
 eva_df = pd.read_json(input_file, convert_dates=['date'])
 eva_df['eva'] = eva_df['eva'].astype(float)
-eva_df.dropna(axis=0, inplace=True)
-eva_df.sort_values('date', inplace=True)
 
+# Remove rows with missing values
+eva_df.dropna(axis=0, inplace=True)
+
+# Sort the DataFrame by the 'date' column in ascending order
+eva_df.sort_values('date', inplace=True)
 eva_df.to_csv(output_file, index=False)
 
+# Convert 'duration' column (in HH:MM format) to total hours as a float
 eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
+# Calculate cumulative time spent in space over the years
 eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
 plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
 plt.xlabel('Year')
